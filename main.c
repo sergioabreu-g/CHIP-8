2618d3c60275076e59791c480a636b93a993aaf0
Sergio Abreu García <contact@sergioabreu.me>
2024-01-11 21:47:23 +0100

    More SDL basic code.

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
 struct Chip {
   /// General purpose registers.
@@ -24,8 +27,65 @@ struct Chip {
   unsigned char _sc;
 };
 
-int main() {
-  SDL_CreateWindow("CHIP-8 Emulator", 0, 0, 64, 32, 0);
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-  while (1) {}
+int main() {
+  int init_res = SDL_Init(SDL_INIT_VIDEO);
+
+  if(init_res < 0)
+  {
+      printf("SDL could not be initialized!\n"
+             "SDL_Error: %s\n", SDL_GetError());
+      return init_res;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("CHIP-8 Emulator", SDL_WINDOW_RESIZABLE, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  if(!renderer)
+  {
+      printf("Renderer could not be created!\n"
+             "SDL_Error: %s\n", SDL_GetError());
+      return -1;
+  }
+
+    // Event loop exit flag
+    bool quit = false;
+
+    // Event loop
+    while(!quit)
+    {
+        SDL_Event e;
+
+        // Wait indefinitely for the next available event
+        SDL_WaitEvent(&e);
+
+        // User requests quit
+        if(e.type == SDL_QUIT)
+        {
+            quit = true;
+        }
+
+        // Initialize renderer color white for the background
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
+    }
+
+    // Destroy renderer
+    SDL_DestroyRenderer(renderer);
+
+  // Destroy window
+  SDL_DestroyWindow(window);
+
+  // Quit SDL
+  SDL_Quit();
+
+  return 0;
 }
